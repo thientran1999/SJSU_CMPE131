d8a01066d04e0e385a32d8eaebeecf48ad5604ec
thientran1999 <thanhthientran1018@gmail.com>
2021-03-06 01:06:36 -0800

    another fix for investment.py

--- investment.py
+++ investment.py
@@ -14,16 +14,14 @@ value: float
 def calculate_apr(principal, interest_rate, years):
     i = 1
     value = 0.0
+    if isinstance(principal, int):
+        return 'false'
+    elif isinstance(interest_rate, float):
+        return 'false'
+    elif isinstance(years, int):
+        return 'false'
     while i <= years:
         value = value + principal * (1 + interest_rate)
         i+=1
     return value
-while True:
-    try:
-        principal = int(input('Enter principal: '))
-        interest_rate = float(input('Enter interest rate: '))
-        years = int(input('Enter years: '))
-        break
-    except ValueError:
-        print('False')
-print ('Value: ', calculate_apr(principal, interest_rate, years))
+
